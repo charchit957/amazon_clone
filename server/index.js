0b13868127dd2ec604da8e2cf117da2bd2857f25
charchit-agarwal <charchit957@gmail.com>
2023-06-02 16:38:34 +0530

    NODEMON installed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 console.log("hello")
+
+const PORT = 3000
+//import
+const express = require('express')
+const app=express()
+
+//CREATING AN API
+//GET,PUT,POST,DELETE,UPDATE = > CRUD
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`connected at ${PORT}`)
+})
